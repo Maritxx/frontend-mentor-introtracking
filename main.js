117b92ad7d87377424588c9fda80e1d8963d6172
Maritxx <maritdevries1@live.nl>
2022-07-28 17:04:16 +0200

    Added Js for button click.

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ $(window).on("load resize orientationchange", function () {
 //The function within the .on action specifies that it needs a function to run.
 //Runs the pageHeight function when the page gets loaded, resized or if the orientation (portrait or landscape) is changed. 
 
+
+
+
+
+
+function expandMenu() {
+	$(".menu-mobile__dropdown").toggleClass('expand');
+}
+//Adds the class of 'expand' when it does not currently have this. Removes the class when it does.
+
+
+function navToggle() {
+	$("#nav-icon").toggleClass('open');
+}
+//Adds the class of 'open' when it does not currently have this. Removes the class when it does.
+
+
+//MIGHT NEED $(function() {
+$("button#menu-button").click(function() {
+	expandMenu();
+	navToggle();
+});
+//MIGHT NEED });
+//Runs the functions 'expandMenu' and 'navToggle' when the button gets clicked.
